c2f9c5b359268442f1b14164cd0da3afa68f79c7
Gregor Adams <greg@pixelass.com>
2022-01-21 22:10:02 +0100

    refactor: simplify redirect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@
 const nextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/characters",
+				destination: "/characters/1",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
